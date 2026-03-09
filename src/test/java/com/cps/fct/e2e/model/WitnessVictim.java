027399201951f9e6991506f38135417b68a9c654
Venkata.Mannepalli <venkata.mannepalli@cps.gov.uk>
2026-03-09 11:30:13 +0000

    Modified Class to Record

--- src/test/java/com/cps/fct/e2e/model/WitnessVictim.java
+++ src/test/java/com/cps/fct/e2e/model/WitnessVictim.java
@@ -1,32 +1,13 @@
 package com.cps.fct.e2e.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
 import java.util.List;
 
-@Getter
-@AllArgsConstructor
-public class WitnessVictim {
-
-
-    private final List<String> witnessId;
-    private final List<String> witnessChildId;
-    private final List<String> witnessExpertId;
-    private final List<String> witnessPrisonerId;
-    private final List<String> witnessInterpreterId;
-    private final List<String> witnessVulnerableId;
-    private final List<String> witnessPoliceId;
-    private final List<String> witnessProfessionalId;
-    private final List<String> witnessIntimidatedId;
-    private final List<String> victimId;
-    private final List<String> victimChildId;
-    private final List<String> victimExpertId;
-    private final List<String> victimPrisonerId;
-    private final List<String> victimInterpreterId;
-    private final List<String> victimVulnerableId;
-    private final List<String> victimPoliceId;
-    private final List<String> victimProfessionalId;
-    private final List<String> victimIntimidatedId;
-
+public record WitnessVictim(List<String> witnessId, List<String> witnessChildId, List<String> witnessExpertId,
+                            List<String> witnessPrisonerId, List<String> witnessInterpreterId,
+                            List<String> witnessVulnerableId, List<String> witnessPoliceId,
+                            List<String> witnessProfessionalId, List<String> witnessIntimidatedId,
+                            List<String> victimId, List<String> victimChildId, List<String> victimExpertId,
+                            List<String> victimPrisonerId, List<String> victimInterpreterId,
+                            List<String> victimVulnerableId, List<String> victimPoliceId,
+                            List<String> victimProfessionalId, List<String> victimIntimidatedId) {
 }

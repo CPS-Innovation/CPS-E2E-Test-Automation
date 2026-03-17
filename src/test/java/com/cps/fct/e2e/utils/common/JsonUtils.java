c99b543050808a6329d49d964e353002a97487fa
Venkata.Mannepalli <venkata.mannepalli@cps.gov.uk>
2026-03-17 13:18:58 +0000

    @S5_15943 InProgress_1

--- src/test/java/com/cps/fct/e2e/utils/common/JsonUtils.java
+++ src/test/java/com/cps/fct/e2e/utils/common/JsonUtils.java
@@ -72,14 +72,18 @@ public class JsonUtils {
         return rawArray.stream()
                 .map(Object::toString)
                 .collect(Collectors.toList());
-
     }
 
-    public static String extractCategoryFromJson(String jsonBody, String jsonPathExpression) {
+    public static List<Object> extractObjectListFromJson(String jsonBody, String jsonPathExpression) {
+        JSONArray rawArray = JsonPath.read(jsonBody, jsonPathExpression);
+        return rawArray.stream()
+                .map(Object::toString)
+                .collect(Collectors.toList());
+    }
+
+    public static String extractFromJson(String jsonBody, String jsonPathExpression) {
         JSONArray rawArray = JsonPath.read(jsonBody, jsonPathExpression);
         return rawArray.getFirst().toString();
-
-
     }
 
     public static String toJsonString(Map<String, Object>payload) {

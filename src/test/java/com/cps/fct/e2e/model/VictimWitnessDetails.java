c827e8326f7bc7357afce7dc6996ab64663ff12e
Murali Govindachar (Kainos) <Murali.Govindachar@cps.gov.uk>
2026-03-18 10:29:27 +0000

    Revert "VCA 15300 S1 Details added"

--- src/test/java/com/cps/fct/e2e/model/VictimWitnessDetails.java
+++ src/test/java/com/cps/fct/e2e/model/VictimWitnessDetails.java
@@ -11,10 +11,8 @@ public class VictimWitnessDetails {
     private String surname;
     private String gender;
     private String dateOfBirth;
-    private String ethnicity;
-    private String disability;
-    private String category;
-    private String previousConvictions;
+    private String contactDetailsTitle;
+    private String contactDetailsGender;
     private String contactDetailsPhoneNumber;
     private String contactDetailsMobileNumber;
     private String contactDetailsWorkPhoneNumber;
@@ -26,8 +24,9 @@ public class VictimWitnessDetails {
     private String contactDetailsPostalAddressAddressLine5;
     private String contactDetailsPostalAddressPostcode;
     private String contactDetailsPreferredCorrespondenceLanguage;
-
-
+    private String contactDetailsDisability;
+    private String contactDetailsEthnicity;
+    private String previousConvictions;
     private String justification;
 
 }

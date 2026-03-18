5a892f6805f22a5d68dfc4c75ce52947320494ab
Murali Govindachar (Kainos) <Murali.Govindachar@cps.gov.uk>
2026-03-18 10:29:27 +0000

    Revert "Update Witness added"

--- src/test/java/com/cps/fct/e2e/utils/FileMapping/DCFMapper.java
+++ src/test/java/com/cps/fct/e2e/utils/FileMapping/DCFMapper.java
@@ -34,10 +34,6 @@ public class DCFMapper implements FileMapper {
                 return path.toFile();
             }
 
-            if (normalized.startsWith("witnessDetails")) {
-                return path.toFile();
-            }
-
             if (normalized.startsWith("victim")) {
                 return path.toFile();
             }

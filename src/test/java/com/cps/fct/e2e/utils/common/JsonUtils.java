7cee6737a69943b528f77b3a26cd712db0d17c58
Murali Govindachar (Kainos) <Murali.Govindachar@cps.gov.uk>
2026-03-18 10:29:27 +0000

    Revert "Vca Sc3_progress_3"

--- src/test/java/com/cps/fct/e2e/utils/common/JsonUtils.java
+++ src/test/java/com/cps/fct/e2e/utils/common/JsonUtils.java
@@ -78,8 +78,6 @@ public class JsonUtils {
     public static String extractCategoryFromJson(String jsonBody, String jsonPathExpression) {
         JSONArray rawArray = JsonPath.read(jsonBody, jsonPathExpression);
         return rawArray.getFirst().toString();
-
-
     }
 
     public static String toJsonString(Map<String, Object>payload) {

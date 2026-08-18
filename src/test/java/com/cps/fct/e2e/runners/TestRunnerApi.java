87a24408d034a8d1f453a5f1472d3921a5ea6383
Murali.Govindachar <murali.govindachar@cps.gov.uk>
2026-08-18 13:05:16 +0100

    rebase with develop

--- src/test/java/com/cps/fct/e2e/runners/TestRunnerApi.java
+++ src/test/java/com/cps/fct/e2e/runners/TestRunnerApi.java
@@ -23,8 +23,10 @@ import static io.cucumber.junit.platform.engine.Constants.OBJECT_FACTORY_PROPERT
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.cps.fct.e2e")
 @ConfigurationParameter(
         key = PLUGIN_PROPERTY_NAME,
-        value = "pretty, json:target/cucumber-report/cucumber.json, io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
-)
+        value = "pretty, json:target/cucumber-report/cucumber.json, io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm" )
+//@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@caseReview" )
+@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@vca_api_regression" )
+
 @ConfigurationParameter(
         key = OBJECT_FACTORY_PROPERTY_NAME,
         value = "com.cps.fct.e2e.support.CucumberObjectFactory"

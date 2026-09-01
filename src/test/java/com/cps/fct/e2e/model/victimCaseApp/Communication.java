a507cc20ef4107dfef697020b6ec709b32d9979e
Murali.Govindachar <murali.govindachar@cps.gov.uk>
2026-09-01 16:22:46 +0000

    Scenario s55 to s57

--- src/test/java/com/cps/fct/e2e/model/victimCaseApp/Communication.java
+++ src/test/java/com/cps/fct/e2e/model/victimCaseApp/Communication.java
@@ -20,6 +20,21 @@ public class Communication {
     private String Notes;
     private String PersonName;
     private String PersonRole;
+    private String PhoneNumber;
+    private int CallDirection;
+    private String DateOfContact;
+    private boolean AbleToInformOnCall;
+    private boolean SkippedSMS;
+    private boolean SkippedFollowUp;
+    private String ReasonForNoFollowUp;
+    private String DateTimeSent;
+    private boolean Sent;
+    private String MessageContent;
+    private String GovNotifyAuditGuid;
+    private String ToEmailAddress;
+    private String SentAt;
+    private boolean GovNotifyStatus;
+    private String TimeSent;
     private String CreatedBy;
     private String LastModifiedBy;
 

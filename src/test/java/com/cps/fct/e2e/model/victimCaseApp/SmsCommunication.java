229bc0a82752a246c8c684f9eae3a9c9f780db19
Murali.Govindachar <murali.govindachar@cps.gov.uk>
2026-09-08 12:29:29 +0100

    VCA scenario S25 S24

---
+++ src/test/java/com/cps/fct/e2e/model/victimCaseApp/SmsCommunication.java
@@ -0,0 +1,29 @@
+package com.cps.fct.e2e.model.victimCaseApp;
+
+import lombok.*;
+
+@Data
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class SmsCommunication {
+
+    private int JourneyType;
+//    private String PhoneNumber;
+//    private int CallDirection;
+//    private String DateOfContact;
+//    private boolean AbleToInformOnCall;
+//    private String Notes;
+//    private boolean SkippedSMS;
+//    private boolean SkippedFollowUp;
+//    private String ReasonForNoFollowUp;
+//    private int AttemptOrder;
+    private String DateTimeSent;
+    private boolean Sent;
+    private String MessageContent;
+    private String CreatedBy;
+    private String LastModifiedBy;
+
+}
